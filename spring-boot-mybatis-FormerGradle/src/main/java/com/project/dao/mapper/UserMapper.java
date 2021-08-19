835e3f822d6f1ff7bc90ff7075c000731413e256
Ananya@Ananya7 <ananyachatterjee92@gmail.com>
2021-08-19 22:32:49 +0530

    code changes

--- spring-boot-mybatis-FormerGradle/src/main/java/com/project/dao/mapper/UserMapper.java
+++ spring-boot-mybatis-FormerGradle/src/main/java/com/project/dao/mapper/UserMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
 import com.project.application.model.User;
 
 @Mapper
-public interface UserMp {
+public interface UserMapper {
 
 	@Insert("insert into users(id,name) values(#{userId},#{name})")
 	public int insertUser(User user);
